780fbd17a3b28cb17f92fa792dcf9fc7b5f787ff
sacapuntas767 <256408443+sacapuntas767@users.noreply.github.com>
2026-03-09 22:33:17 -0400

    update

---
+++ public/script.js
@@ -0,0 +1,20 @@
+//get client data using navigator
+console.log(window.navigator.cookieEnabled);
+console.log(window.navigator.onLine);
+console.log(navigator.appVersion);
+console.log(navigator.userAgent)
+console.log(navigator.platform);
+
+//get window metadata using window
+console.log(window.location.href);//get full url
+console.log(window.location.protocol);
+console.log(window.location.hostname);
+
+function redirect(url){
+  window.location.assign(url);//redirects the page to another url
+}
+
+window.onload = function(event){
+   console.log("Page has loaded");
+   //do other javascript stuff here
+}
